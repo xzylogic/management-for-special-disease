c7e18d07fb8e00c4f49f3c2f6953db668927198b
wangbing <120334644@qq.com>
2017-07-31 11:26:57 +0800

    operation

--- src/app/pci/manage-operation-push/_entity/operationPush.entity.ts
+++ src/app/pci/manage-operation-push/_entity/operationPush.entity.ts
@@ -1,16 +1,22 @@
-export class Operation {
+export class OperationPush {
   id: number;
   content: string;
+  operator: string;
   pushTime: string;
   pushUrl: string;
-  type: number;
+  skip: boolean;
+  send: boolean;
+  type: number; // 患者或者医生端
 
 
-  constructor(obj?: Operation) {
+  constructor(obj?: OperationPush) {
     this.id = obj && obj.id || 0;
     this.content = obj && obj.content || '';
     this.pushTime = obj && obj.pushTime || '';
+    this.operator = obj && obj.operator || '';
     this.pushUrl = obj && obj.pushUrl || '';
+    this.skip = obj && obj.skip || true;
+    this.send = obj && obj.send || true;
     this.type = obj && obj.type || 0;
   }
 }
--- src/app/pci/manage-operation-push/operation-push.module.ts
+++ src/app/pci/manage-operation-push/operation-push.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [{
   ],
   providers: [
     OperationPushService,
-    OperationPushTableService
+    OperationPushTableService,
   ]
 })
 export class OperationPushModule {
