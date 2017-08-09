57f97c3fa724a86435aa807b0b18f204bf5b0969
VioletQQY <forviolet771@gmail.com>
2017-08-09 11:33:32 +0800

    ad maxlength del

--- src/app/pci/manage-ad/ad-patient/ad-patient-edit/ad-patient-edit.component.ts
+++ src/app/pci/manage-ad/ad-patient/ad-patient-edit/ad-patient-edit.component.ts
@@ -65,7 +65,6 @@ export class AdPatientEditComponent implements OnInit {
       title: new FormText({
         key: 'title',
         label: '广告标题',
-        maxlength: 24,
         value: data && data.title || ''
       }),
       subTitle: new FormText({
