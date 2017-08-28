e995d1f2396cd31b74cc2198d3401fc3d3aab1c5
VioletQQY <forviolet771@gmail.com>
2017-08-28 09:40:22 +0800

    Object.assign pplyfill

--- src/app/pci/_store/api/api.reducer.ts
+++ src/app/pci/_store/api/api.reducer.ts
@@ -4,6 +4,14 @@ import {
   ApiAction, DataChangeAction,
   PageChangeAction, TabChangeAction
 } from './api.action';
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 
 export function createReducer(key, initPage: Array<number>, initData: any) {
   return function apiReducer(state: IApiState<any> = {
@@ -25,19 +33,19 @@ export function createReducer(key, initPage: Array<number>, initData: any) {
 }
 
 function handleTabChangeAction(state: IApiState<any>, action: TabChangeAction) {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.tab = action.payload;
   return stateCopy;
 }
 
 function handlePageChangeAction(state: IApiState<any>, action: PageChangeAction) {
-  const stateCopy = Object.assign(state);
+  const stateCopy =__assign(state);
   stateCopy.page = action.payload;
   return stateCopy;
 }
 
 function handleDataChangeAction(state: IApiState<any>, action: DataChangeAction) {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.data = action.payload;
   return stateCopy;
 }
--- src/app/pci/_store/main.reducer.ts
+++ src/app/pci/_store/main.reducer.ts
@@ -2,6 +2,14 @@ import { Action } from 'redux';
 import { IMainState } from './main.state';
 import { DelAdminAction, InitNavAction, MainAction, SetAdminAction, UpdateTagAction } from './main.action';
 import { NAVBARS } from './static';
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 
 export function MainReducer(state: IMainState = {
   adminId: 0,
@@ -23,21 +31,21 @@ export function MainReducer(state: IMainState = {
 }
 
 function handleSetAdminAction(state: IMainState, action: SetAdminAction): IMainState {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.adminId = action.payload.id;
   stateCopy.adminName = action.payload.name;
   return stateCopy;
 }
 
 function handleDelAdminAction(state: IMainState, action: DelAdminAction): IMainState {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.adminId = 0;
   stateCopy.adminName = '';
   return stateCopy;
 }
 
 function handleInitNavAction(state: IMainState, action: InitNavAction): IMainState {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.navigation.forEach(obj => {
     obj.active = false;
     obj.open = false;
@@ -58,7 +66,7 @@ function handleInitNavAction(state: IMainState, action: InitNavAction): IMainSta
 }
 
 function handleUpdateTagAction(state: IMainState, action: UpdateTagAction): IMainState {
-  const stateCopy = Object.assign(state);
+  const stateCopy = __assign(state);
   stateCopy.navigation.map(sidebars => {
     if (sidebars.subBars) {
       sidebars.subBars.forEach(subObj => {
