4206ca352c60fce8e0abc1b0297a8f0d5ee104b6
VioletQQY <forviolet771@gmail.com>
2018-02-26 14:59:55 +0800

    hospital table change

--- src/app/pci/manage-basic-data/hospital/_service/hospital-table.service.ts
+++ src/app/pci/manage-basic-data/hospital/_service/hospital-table.service.ts
@@ -21,11 +21,19 @@ export class HospitalTableService {
         key: 'name',
       }),
       new TableTitle({
+        name: '医生数量',
+        key: 'num',
+      }),
+      new TableTitle({
         name: '地理位置',
         key: 'geography',
         minwidth: 85,
         option: '查看',
         controlType: ControlType.button,
+      }),
+      new TableTitle({
+        name: '状态',
+        key: 'enableName',
       }),
       new TableTitle({
         name: '编辑',
