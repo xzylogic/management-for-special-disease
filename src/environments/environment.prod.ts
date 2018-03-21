e519b1d6c7e5f5433ffe17fae8d744bd14de1a64
VioletQQY <forviolet771@gmail.com>
2018-03-21 13:55:19 +0800

    modify http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
 
 export const app = {
   pci: {
-    BASE_URL: 'http://www.qcxin.com/pci-operation/',
-    COMMON_URL: 'http://www.qcxin.com/pro-health/',
-    UPLOAD_URL: 'http://www.qcxin.com/pci-operation/api/upload'
+    BASE_URL: 'https://www.qcxin.com/pci-operation/',
+    COMMON_URL: 'https://www.qcxin.com/pro-health/',
+    UPLOAD_URL: 'https://www.qcxin.com/pci-operation/api/upload'
   },
   kidney: {
-    BASE_URL: 'http://www.qcxin.com/kidney-backend-test/',
-    CAN_URL: 'http://www.qcxin.com/kidney-scheduler-test/',
-    COMMON_URL: 'http://www.qcxin.com/pro-health/',
-    UPLOAD_URL: 'http://www.qcxin.com/pci-operation/api/upload'
+    BASE_URL: 'https://www.qcxin.com/kidney-backend-test/',
+    CAN_URL: 'https://www.qcxin.com/kidney-scheduler-test/',
+    COMMON_URL: 'https://www.qcxin.com/pro-health/',
+    UPLOAD_URL: 'https://www.qcxin.com/pci-operation/api/upload'
   }
 };
