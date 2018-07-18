7a1f2e175ecdf56b352b370ce997681e0b983c81
VioletQQY <forviolet771@gmail.com>
2018-07-18 20:29:46 +0800

    add image compress

--- src/app/libs/dform/component/lib-input/lib-input-file.ts
+++ src/app/libs/dform/component/lib-input/lib-input-file.ts
@@ -54,11 +54,24 @@ export class LibInputFileComponent implements OnInit {
   // 上传图片操作
   uploadChange(files) {
     const myForm = new FormData();
-    const fileList = files.target.files
-    for (let i = 0; i < fileList.length; i ++) {
-      myForm.append('file', fileList[i]);
+    const fileList = files.target.files;
+    for (let i = 0; i < fileList.length; i++) {
+      // console.log(fileList[i]);
+      if (fileList[i].size > 5 * 1024 * 1024) {
+        this.photoCompress(fileList[i], 1536, (value) => {
+          // console.log(value);
+          myForm.append('file', value);
+          this.fileUpload(myForm)
+        })
+      } else {
+        myForm.append('file', fileList[i]);
+        this.fileUpload(myForm)
+      }
     }
-    this.uploadService.upload(this.data.url, myForm)
+  }
+
+  fileUpload(form) {
+    this.uploadService.upload(this.data.url, form)
       .subscribe(res => {
         if (res.code === 0) {
           HintDialog('上传图片成功！', this.dialog);
@@ -68,7 +81,7 @@ export class LibInputFileComponent implements OnInit {
             if (!this.value) {
               this.value = [];
             }
-            for (let i = 0; i < res.data.length; i ++) {
+            for (let i = 0; i < res.data.length; i++) {
               this.value.push(res.data[i]);
             }
           }
@@ -106,4 +119,75 @@ export class LibInputFileComponent implements OnInit {
   change() {
     this.valueChange.emit(this.value);
   }
+
+  /**
+   * 图片压缩
+   * @param file 文件
+   * @param width 压缩宽度
+   * @param callback
+   */
+  photoCompress(file, width, callback) {
+    let ready = new FileReader();
+    ready.readAsDataURL(file);
+    ready.onload = (e) => {
+      let re = e.target['result'];
+      this.canvasDataURL(re, width, callback);
+    }
+  }
+
+  canvasDataURL(imagePath, width, callback) {
+    let img = new Image();
+    img.src = imagePath;
+    img.onload = (e) => {
+      // 图片原始尺寸
+      let originWidth = e.target['width'], originHeight = e.target['height'];
+      // 最大尺寸限制
+      let maxWidth = width || 800, maxHeight = width || 800;
+      // 目标尺寸
+      let targetWidth = originWidth, targetHeight = originHeight;
+      // 图片尺寸超过 [width] x [width] 的限制
+      if (originWidth > maxWidth || originHeight > maxHeight) {
+        if (originWidth / originHeight > maxWidth / maxHeight) {
+          // 更宽，按照宽度限定尺寸
+          targetWidth = maxWidth;
+          targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+        } else {
+          targetHeight = maxHeight;
+          targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+        }
+      }
+
+      // 缩放图片需要的canvas
+      let canvas = document.createElement('canvas');
+      let context = canvas.getContext('2d');
+      // canvas对图片进行缩放
+      canvas.width = targetWidth;
+      canvas.height = targetHeight;
+      // 清除画布
+      context.clearRect(0, 0, targetWidth, targetHeight);
+      // 图片压缩
+      context.drawImage(img, 0, 0, targetWidth, targetHeight);
+
+      canvas.toBlob((blob) => {
+        callback(blob);
+      })
+    }
+  }
+
+  /**
+   * 将以base64的图片url数据转换为Blob
+   * @param urlData 用url方式表示的base64图片数据
+   */
+  convertBase64UrlToBlob(urlData) {
+    let arr = urlData.split(',');
+    let mime = arr[0].match(/:(.*?);/)[1];
+    let bstr = atob(arr[1]);
+    let n = bstr.length;
+    let u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type: mime});
+  }
+
 }
