eaebd06db5c297722268a29da266816045c6a320
xzy <1062186337@qq.com>
2018-08-27 14:41:08 +0800

    患者信息管理修改20180827

--- src/app/pci/manage-user/user/_service/user.service.ts
+++ src/app/pci/manage-user/user/_service/user.service.ts
@@ -78,18 +78,15 @@ export class UserService {
   //  * @param {number} size    [description]
   //  */
   getUsers(key: string, bind: any, registrationTime: any, page: number, size: number, hospitalId?: number, type?: number) {
-    let query = `?keyword=${key}&binding=${bind}&registrationTime=${registrationTime}&page=${page}&size=${size}&type=${type}`
-    // if (type) {
-    //   query += `&type=${type}`
-    // }
+    let query = `?keyword=${key}&binding=${bind}&registrationTime=${registrationTime}&page=${page}&size=${size}`;
+    if (type || type === 0) {
+      query += `&type=${type}`
+    }
     if (type !== 0 && hospitalId) {
       query += `&hospitalId=${hospitalId}`;
     }
-    if (type == 0) {
+    if (type === 0) {
       query += `&sceneId=${hospitalId}`
-    }
-    if(size === 2000){
-      query = `?keyword=${key}&binding=${bind}&registrationTime=${registrationTime}&page=${page}&size=${size}`
     }
     return this.httpService.get(
       `${this.app.pci.BASE_URL}${PATH.userList}${query}`
