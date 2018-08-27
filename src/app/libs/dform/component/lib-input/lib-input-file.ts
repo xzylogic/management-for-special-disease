cf556357b007f2ba6a7f7d0215d0b298066225f5
VioletQQY <forviolet771@gmail.com>
2018-08-27 14:17:48 +0800

    modify user upload file upload

--- src/app/libs/dform/component/lib-input/lib-input-file.ts
+++ src/app/libs/dform/component/lib-input/lib-input-file.ts
@@ -5,6 +5,60 @@ import { HttpService } from '../../../_service/http.service';
 import { HintDialog } from '../../../dmodal/dialog.component';
 import { FormFile } from '../../_entity/form-file';
 
+function photoCompress(file, width) {
+  return new Promise(function (resolve, reject) {
+    if (file && file.size > 3 * 1024 * 1024) {
+      let ready = new FileReader();
+      ready.readAsDataURL(file);
+      ready.onload = (e) => {
+        let re = e.target['result'];
+        canvasDataURL(re, width, resolve);
+      }
+    } else {
+      resolve(file);
+    }
+  })
+}
+
+function canvasDataURL(imagePath, width, callback) {
+  let img = new Image();
+  img.src = imagePath;
+  img.onload = (e) => {
+    // 图片原始尺寸
+    let originWidth = e.target['width'], originHeight = e.target['height'];
+    // 最大尺寸限制
+    let maxWidth = width || 800, maxHeight = width || 800;
+    // 目标尺寸
+    let targetWidth = originWidth, targetHeight = originHeight;
+    // 图片尺寸超过 [width] x [width] 的限制
+    if (originWidth > maxWidth || originHeight > maxHeight) {
+      if (originWidth / originHeight > maxWidth / maxHeight) {
+        // 更宽，按照宽度限定尺寸
+        targetWidth = maxWidth;
+        targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+      } else {
+        targetHeight = maxHeight;
+        targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+      }
+    }
+
+    // 缩放图片需要的canvas
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    // canvas对图片进行缩放
+    canvas.width = targetWidth;
+    canvas.height = targetHeight;
+    // 清除画布
+    context.clearRect(0, 0, targetWidth, targetHeight);
+    // 图片压缩
+    context.drawImage(img, 0, 0, targetWidth, targetHeight);
+
+    canvas.toBlob((blob) => {
+      callback(blob);
+    })
+  }
+}
+
 @Component({
   selector: 'app-input-file',
   template: `
@@ -55,22 +109,35 @@ export class LibInputFileComponent implements OnInit {
   uploadChange(files) {
     const myForm = new FormData();
     const fileList = files.target.files;
-    for (let i = 0; i < fileList.length; i++) {
-      // console.log(fileList[i].size / (1024 * 1024));
-      if (fileList[i].size > 3 * 1024 * 1024 && this.data.ifCompress) {
-        this.photoCompress(fileList[i], 1360, (value) => {
-          // console.log(value.size / (1024 * 1024));
+    if (this.data.multiple === true) {
+      let filePromise = [];
+      for (let i = 0; i < fileList.length; i++) {
+        filePromise.push(photoCompress(fileList[i], 1360))
+      }
+      Promise.all(filePromise).then(filesout => {
+        console.log(filesout);
+        filesout.map(singleFile => {
+          myForm.append('file', singleFile);
+        });
+        this.fileUpload(myForm);
+      });
+    } else {
+      if (fileList[0] && fileList[0].size > 3 * 1024 * 1024 && this.data.ifCompress) {
+        console.log(fileList[0].size / (1024 * 1024));
+        this.photoCompress(fileList[0], 1360, (value) => {
+          console.log(value.size / (1024 * 1024));
           myForm.append('file', value);
           this.fileUpload(myForm)
         })
       } else {
-        myForm.append('file', fileList[i]);
-        this.fileUpload(myForm)
+        myForm.append('file', fileList[0]);
+        this.fileUpload(myForm);
       }
     }
   }
 
   fileUpload(form) {
+    console.log(form);
     this.uploadService.upload(this.data.url, form)
       .subscribe(res => {
         if (res.code === 0) {
@@ -131,7 +198,7 @@ export class LibInputFileComponent implements OnInit {
     ready.readAsDataURL(file);
     ready.onload = (e) => {
       let re = e.target['result'];
-      this.canvasDataURL(re, width, callback);
+      canvasDataURL(re, width, callback);
     }
   }
 
